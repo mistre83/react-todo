c0c56505dc67bfd32ee2982322504bb07c940698
mistre83 <mistre83@gmail.com>
2016-05-23 22:56:55 +0200

    Refactor app to work with local storage

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -19,6 +19,24 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate add todos action object', () => {
+    var todos =[{
+      id: '111',
+      text: 'anything',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 33330
+    }];
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+
+    var res = actions.addTodos(todos);
+
+    expect(res).toEqual(action);
+  });
+
   it('should generate add todo action', () => {
     var action = {
       type: 'ADD_TODO',
